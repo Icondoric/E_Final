2482966ac0c3dc1a7e2d37dd0262017b7683593b
Icondoric <iconhu.icc@gmail.com>
2023-11-30 12:01:45 -0400

    acabe

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import { infoExrcise } from './assets/ExerciseAPI';
 function App() {
 
   return (
+    
     <div className="App">
+            <h3>Exercícios</h3>
+
       <ExerciseCard exercises={infoExrcise} />
     </div>
   )
